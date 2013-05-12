c4c8111b7159b20144f65a3648e2c2ad4be1a0fe
Jesse Merlin <jwmerlin@gmail.com>
2013-05-12 17:20:06 -0400

    Adding timeout 0 to phpqatools

--- puppet/modules/jenkins-php/manifests/init.pp
+++ puppet/modules/jenkins-php/manifests/init.pp
@@ -7,18 +7,10 @@ class jenkins-php {
         enable => false,
     }
 
-    exec { 'pear-auto-discover':
-        path    => '/usr/bin:/usr/sbin:/bin',
-        onlyif  => 'test "`pear config-get auto_discover`" = "0"',
-        command => 'pear config-set auto_discover 1 system',
-        require => Class['php'],
-    }
-
-    exec { 'install-phpqatools':
-        unless => '/usr/bin/pear info pear.phpqatools.org/phpqatools',
-        command => '/usr/bin/pear install pear.phpqatools.org/phpqatools',
-        timeout => 0,
-        require => [Class['php'], Exec['pear-auto-discover']],
+    php::pear::module { 'phpqatools':
+        repository  => 'pear.phpqatools.org',
+        use_package => 'no',
+        timeout     => 0,
     }
 
     package { "php-xml":
